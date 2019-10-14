dd2b12a2c302cbcd7136f9e283b6dcb5eaef8b6a
黄彪 <191059014@qq.com>
2019-10-14 09:55:16 +0800

    架构

--- cp-dao/src/main/java/com/hb/cp/dao/base/SqlHelper.java
+++ cp-dao/src/main/java/com/hb/cp/dao/base/SqlHelper.java
@@ -13,8 +13,21 @@ import java.util.Map;
  */
 public class SqlHelper {
 
+    /**
+     * 记录状态字段名
+     */
     private static final String RECORDSTATUS = "recordStatus";
 
+    /**
+     * 构建查询语句
+     *
+     * @param tableName  表名
+     * @param conditions 条件集合
+     * @param sort       排序
+     * @param startRow   开始行数
+     * @param pageNum    每页条数
+     * @return sql
+     */
     public static String buildSelectSelectiveSql(String tableName, Map<String, Object> conditions, String sort, Integer startRow, Integer pageNum) {
         StringBuilder sb = new StringBuilder();
         sb.append(makeSelectBase(tableName));
@@ -28,31 +41,64 @@ public class SqlHelper {
         return sb.toString();
     }
 
-    private static String makeSelectCondition(String key, Object value) {
+    /**
+     * 组装条件
+     *
+     * @param key   字段名
+     * @param value 字段值
+     * @return 条件sql片段
+     */
+    public static String makeSelectCondition(String key, Object value) {
         if (value != null) {
             return " and " + key + "=" + "#{params." + key + "}";
         }
         return "";
     }
 
-    private static String makeSelectBase(String tableName) {
+    /**
+     * 组装最基本的select语句片段
+     *
+     * @param tableName 表名
+     * @return 基本的select语句
+     */
+    public static String makeSelectBase(String tableName) {
         return "select * from " + tableName + " where " + RECORDSTATUS + "=" + RecordStateEnum.VALID.getValue();
     }
 
-    private static String makeSelectSort(String sort) {
+    /**
+     * 组装查询排序条件sql片段
+     *
+     * @param sort 排序
+     * @return 排序的sql片段
+     */
+    public static String makeSelectSort(String sort) {
         if (sort != null && !"".equals(sort)) {
             return " order by " + sort;
         }
         return "";
     }
 
-    private static String makePages(Integer startRow, Integer pageNum) {
+    /**
+     * 组装分页条件sql片段
+     *
+     * @param startRow 开始行
+     * @param pageNum  每页条数
+     * @return 分页sql片段
+     */
+    public static String makePages(Integer startRow, Integer pageNum) {
         if (startRow != null && pageNum != null) {
             return " limit #{startRow},#{pageNum} ";
         }
         return "";
     }
 
+    /**
+     * 构建插入sql语句
+     *
+     * @param tableName 表名
+     * @param property  字段集合
+     * @return 插入sql语句
+     */
     public static String buildInsertSelectiveSql(String tableName, Map<String, Object> property) {
         StringBuilder sb = new StringBuilder("insert into " + tableName);
         StringBuilder cloumSb = new StringBuilder(" (");
@@ -72,6 +118,14 @@ public class SqlHelper {
         return sb.toString();
     }
 
+    /**
+     * 构建更新sql语句
+     *
+     * @param tableName  表名
+     * @param property   字段集合
+     * @param conditions 条件集合
+     * @return 更新sql语句
+     */
     public static String buildUpdateSelectiveSql(String tableName, Map<String, Object> property, Map<String, Object> conditions) {
         StringBuilder sb = new StringBuilder("update " + tableName + " set ");
         StringBuilder cloumSb = new StringBuilder();
@@ -97,6 +151,13 @@ public class SqlHelper {
         return sb.toString();
     }
 
+    /**
+     * 构建删除sql语句
+     *
+     * @param tableName  表名
+     * @param conditions 条件集合
+     * @return 删除sql语句
+     */
     public static String buildDeleteSelectiveSql(String tableName, Map<String, Object> conditions) {
         StringBuilder sb = new StringBuilder("update " + tableName + " set " + RECORDSTATUS + "=" + RecordStateEnum.INVALID.getValue());
         StringBuilder whereSb = new StringBuilder(" where 1=1");
@@ -112,6 +173,13 @@ public class SqlHelper {
         return sb.toString();
     }
 
+    /**
+     * 构建查询总条数sql语句
+     *
+     * @param tableName  表名
+     * @param conditions 条件集合
+     * @return 查询总条数sql语句
+     */
     public static String buildSelectCountSelectiveSql(String tableName, Map<String, Object> conditions) {
         StringBuilder sb = new StringBuilder();
         sb.append("select count(*) from " + tableName + " where " + RECORDSTATUS + "=" + RecordStateEnum.VALID.getValue());
