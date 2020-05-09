346f69613a48233ba25d44ff0709a9b89701034e
黄彪 <191059014@qq.com>
2020-05-09 15:38:36 +0800

    测试simple-mybatis

--- cp-web/src/main/java/com/hb/cp/web/test/TestController.java
+++ cp-web/src/main/java/com/hb/cp/web/test/TestController.java
@@ -4,6 +4,7 @@ import com.hb.mybatis.base.DmlMapper;
 import com.hb.mybatis.helper.QueryCondition;
 import com.hb.mybatis.helper.QueryType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,11 +24,21 @@ public class TestController {
     @Autowired
     private DmlMapper dmlMapper;
 
+    @Value("${username}")
+    private String username;
+
+    @Value("${simple.mybatis.result.isHumpMapping}")
+    private String isHumpMapping;
+
     @GetMapping("/testSimpleMybatis")
     public void testSimpleMybatis() {
-        QueryCondition query = QueryCondition.build("")
-                .addCondition(QueryType.LIKE, "coupon_name", "优惠券");
+        QueryCondition query = QueryCondition.build("coupons_config")
+                .addCondition(QueryType.LIKE, "coupon_name", "优惠券")
+                .orderBy("gmt_create desc");
         List<CouponConfigDO> list = dmlMapper.dynamicSelect(CouponConfigDO.class, query);
+        for (CouponConfigDO couponConfigDO : list) {
+            System.out.println(couponConfigDO);
+        }
         System.out.println(list.size());
     }
 
